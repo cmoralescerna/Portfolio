1b26685f619bd181fba4c6c3814148c3cda4b1af
Catherine <cmoralescerna22@gmail.com>
2026-01-20 15:22:03 +0100

    Complete hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import { ArrowDown } from "lucide-react";
+
 export const HeroSection = () => {
     return (
         <section 
@@ -7,11 +9,26 @@ export const HeroSection = () => {
             <div className="container max-w-4xl mx-autio text-center z-10">
                 <div className="space-y-6">
                     <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-                        <span> Hi, I'm</span>
-                        <span> Catherine</span>
-                        <span> Morales Cerna</span>
+                        <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+                        <span className="text-primary opacity-0 animate-fade-in-delay-1"> Catherine</span>
+                        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Morales Cerna</span>
                     </h1>
+                    <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    I am a student studying Software Engineering 
+                    at Gothenburg University. I enjoy web development, solving complex problems, and building an efficient system architecture!    
+                    </p>
+
+                    <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                        <a href="#projects" className="cosmic-button">
+                            View My Projects
+                        </a>
+                    </div>
                 </div>
+            </div>
+
+            <div className="absolute bottom-8 left-1/2 transofmr -translate-x-1/2 flex flex-col items-center animate-bounce">
+                <span className="text-sm text-muted-foreground mb-2">Scroll</span>
+                <ArrowDown className="h-5 w-5 text-primary"/>
             </div>
         </section>
 )}
